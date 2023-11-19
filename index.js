5af617316c541d31c7370bcab09b6cb1947f07cd
yugbathla30 <yug.bathla30@gmail.com>
2023-11-19 15:29:41 +0530

    final last commit

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-const deleteBtn= document.getElementById("delete-btn") 
-const tabBtn= document.getElementById("savetab-btn")
-
+const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
