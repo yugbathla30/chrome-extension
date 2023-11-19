2b80ffb13186ac2becb75c529ff584efa5c4efe1
yugbathla30 <yug.bathla30@gmail.com>
2023-11-19 15:09:43 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn= document.getElementById("delete-btn") 
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener('dblclick', function(){
+    if (localStorage.length > 0 ) {
+        localStorage.clear();
+        myLeads = []
+        renderLeads()
+    } 
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
